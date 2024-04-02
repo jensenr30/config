0e53e09a47905beb2f13f0aa01723a29b65f075c
Ryan Jensen <ryan.jensen@packetdigital.com>
2024-04-02 14:15:34 -0500

    gitslam!

--- .jensenr30/install/git.sh
+++ .jensenr30/install/git.sh
@@ -3,7 +3,17 @@
 git config --global user.name "Ryan Jensen"
 #git config --global user.email jensenr30@gmail.com
 git config --global core.editor e
-git config --global core.autocrlf input
+
+if [ "$sysname" == "linux" ]; then
+    echo "$sysname line endings"
+    git config --global core.autocrlf input
+elif [ "$sysname" == "windows" ]; then
+    echo "$sysname line endings"
+    git config --global core.autocrlf true
+else
+    echo "unknown which line endings to use for sysname: $sysname"
+fi
+    
 git config --global core.excludesFile ~/.gitignore_global
 
 git config --global pull.rebase false
