c48b35ec8615ff93da3d15abd993ff236f028b1d
Ryan Jensen <jensenr30@gmail.com>
2024-07-05 09:37:56 -0500

    gitslam!

--- .alias.sh
+++ .alias.sh
@@ -78,6 +78,7 @@ alias cdi="cd $CFGDIR/install"
 alias cdid="cd $CFGDIR/install/$distro"
 # quick cd into nas storage
 alias cdn='cl /nas/safe'
+alias tri='cd ~/repos/triangulite && ide'
 
 
 ################################################################################
--- .bash_profile
+++ .bash_profile
@@ -1,9 +1,3 @@
-#
-# ~/.bash_profile
-#
-
-source ~/.profile
-
 [[ -f ~/.bashrc ]] && . ~/.bashrc
 
 if [ -e /home/ryan/.nix-profile/etc/profile.d/nix.sh ]; then . /home/ryan/.nix-profile/etc/profile.d/nix.sh; fi
