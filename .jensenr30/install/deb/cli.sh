06d54d6544e69cb1407e0b35b6bca42873738e67
Ryan Jensen <jensenr30@gmail.com>
2024-11-28 16:54:14 +0000

    gitslam!

--- .jensenr30/install/deb/cli.sh
+++ .jensenr30/install/deb/cli.sh
@@ -2,7 +2,7 @@
 # settings and cli programs for every linux system
 sudo apt update
 
-i='sudo apt install -y'
+$i='sudo apt install -y'
 
 # coreutils replacements / enhancements
 $i bat ripgrep fd-find 7zip exa neovim bash-completion atool git git-delta
@@ -11,40 +11,21 @@ $i bat ripgrep fd-find 7zip exa neovim bash-completion atool git git-delta
 $i python3 python3-pip python3-pip python-is-python3
 
 # development tools
-# pip install pre-commit
-# i can-utils
+pip install pre-commit
+$i can-utils
 
 #terminal managers
-# i screen
-# i tmux
+$i screen tmux
 
 # downloaders
-# i wget
-# i curl
-#pip install -U yt-dlp
+$i wget curl
+pip install -U yt-dlp
 
 #system monitor
-# i htop
-# i atop
-# i iftop
-# i nethogs
-# i sysstat
-# i usbtop
-# i bashtop
+$i htop atop iftop nethogs sysstat usbtop bashtop
 
 # networking
-# i net-tools
-# i sshfs
-# i traceroute
-# i arp-scan
+$i net-tools sshfs traceroute arp-scan bridge-utils
 
 # misc
-# i rsync
-# i minicom
-# i fd-find
-# i fzf
-# i numlockx
-# i xsel
-
-# programming
-# i dfu-util
+$i rsync minicom fd-find fzf numlockx xsel
