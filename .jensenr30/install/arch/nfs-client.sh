719c7a9d0ddedbad1b46a8bad4a7b814adc2d48e
Ryan Jensen <jensenr30@gmail.com>
2024-11-29 13:32:49 -0600

    NFS client

--- .jensenr30/install/arch/nfs-client.sh
+++ .jensenr30/install/arch/nfs-client.sh
@@ -13,10 +13,12 @@ realdir='/home/ryan/nas'
 
 $i nfs-utils
 showmount -e server2
-# todo ln -s /nas "$realdir"
-mkdir -p "$realdir/safe"
-mkdir -p "$realdir/junk"
-echo "server2:/nas/safe  $realdir/safe  nfs defaults,timeo=900,retrans=5,_netdev  0 0" >> /etc/fstab
-echo "server2:/nas/junk  $realdir/junk  nfs defaults,timeo=900,retrans=5,_netdev  0 0" >> /etc/fstab
-mount -m server2:/nas/safe "$realdir/safe"
-mount -m server2:/nas/junk "$realdir/junk"
+ln -s "$realdir" /nas
+safe_fstab="server2:$realdir/safe  $realdir/safe  nfs defaults,timeo=900,retrans=5,_netdev  0 0"
+junk_fstab="server2:$realdir/junk  $realdir/junk  nfs defaults,timeo=900,retrans=5,_netdev  0 0"
+echo "$safe_fstab" >> /etc/fstab
+echo "$junk_fstab" >> /etc/fstab
+bat /etc/fstab
+systemctl daemon-reload
+systemctl restart remote-fs.target
+tree -L 2 /nas
