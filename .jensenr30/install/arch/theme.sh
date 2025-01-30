f1e40a069e38f1b231a6e39c16aa5d5d07920f03
Ryan Jensen <jensenr30@gmail.com>
2025-01-29 23:14:57 -0600

    gitslam!

--- .jensenr30/install/arch/theme.sh
+++ .jensenr30/install/arch/theme.sh
@@ -5,6 +5,6 @@ $flat flathub org.kde.KStyle.Adwaita
 $i lxappearance || $i lxappearance-gtk3
 $i qt5ct qt6ct
 $i gnome-themes-extra
-$y adwaita-qt4 adwaita-qt5-git adwaita-qt6-git
+$y adwaita-qt5-git adwaita-qt6-git
 $i papirus-icon-theme
 $i breeze breeze5 breeze-gtk
