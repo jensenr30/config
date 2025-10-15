aae3827196fc8f68db646077d4733bd9b315a705
Ryan Jensen <jensenr30@gmail.com>
2025-10-15 06:30:11 -0500

    gitslam!

--- .jensenr30/install/arch/gaming.sh
+++ .jensenr30/install/arch/gaming.sh
@@ -3,6 +3,6 @@
 source install-helper.sh
 
 $i snes9x retroarch gamemode
-$i --asdeps retroarch-assets-ozone retroarch-gtk
+$i --asdeps retroarch-assets-ozone retroarch-assets-xmb retroarch-gtk 
 
 mkdir -p ~/.config/retroarch/coreinfo
