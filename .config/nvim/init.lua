a489d12973c36fac1e96158ebbb2bbf24266926a
Ryan Jensen <jensenr30@gmail.com>
2025-10-21 10:39:08 -0500

    gitslam!

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -583,8 +583,8 @@ require("lazy").setup({
 			-- your configuration comes here
 			-- for example
 			enabled = false, -- if you want to enable the plugin
-			message_template = " <author> • <summary> • <date> • <<sha>>", -- template for the blame message, check the Message template section for more options
-			date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+			message_template = " <date> • <author> • <summary> • <<sha>>", -- template for the blame message, check the Message template section for more options
+			date_format = "%Y-%m-%d", -- %H:%M:%S  template for the date, check Date format section for more options
 			virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
 			delay = 1200, -- wait before displaying git blame info
 			highlight_group = "BufferInactive", -- NOTE not sure if this will work with color schemes other than vscode...
