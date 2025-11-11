45e91029d0a872575e6531fd84bdb29febf5bdf2
Ryan Jensen <jensenr30@gmail.com>
2025-11-11 01:16:43 -0600

    Fix clangd switch header source command

--- .config/nvim/lua/jensenr30/navigation.lua
+++ .config/nvim/lua/jensenr30/navigation.lua
@@ -10,7 +10,7 @@ vim.keymap.set("n", "<Esc>", close_floating_windows, { desc = "Close all floatin
 vim.keymap.set("n", "<A-h>", "<C-o>", { desc = "Jump to previous location" })
 vim.keymap.set("n", "<A-l>", "<C-i>", { desc = "Jump to next location" })
 
-vim.keymap.set({ "n", "v", "i" }, "<A-o>", "<Esc>:ClangdSwitchSourceHeader<CR>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v", "i" }, "<A-o>", "<Esc>:LspClangdSwitchSourceHeader<CR>", { noremap = true, silent = true })
 
 -- close current buffer without closing the window
 vim.keymap.set("n", "q", function()
